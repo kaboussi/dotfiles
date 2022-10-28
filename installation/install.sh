d73c3aa39e3a1e1be1a1a5f229b01262cb049c24
redouane <elkaboussi@pm.me>
2022-10-28 09:56:22 +0100

    fonts

---
+++ installation/install.sh
@@ -0,0 +1,14 @@
+# Fonts:
+sudo pacman -S noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra
+yay -S nerd-fonts-fira-code 
+
+
+# Editors:
+sudo pacman -S vim neovim
+yay -S visual-studio-code-bin
+
+# Browsers:
+sudo pacman -S firefox
+
+# Media:
+sudo pacman -S mpv
